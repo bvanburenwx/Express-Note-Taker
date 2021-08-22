fce63be2ac88d7031448f8f11e9afb531fff7716
BVanBuren <bvanburenwx@gmail.com>
2021-08-22 15:28:04 -0400

    made the first connection from the backend to the front end with express. Added port listening and ran the front in program on port 3001 and it is succesful.

--- Develop/server.js
+++ Develop/server.js
@@ -10,4 +10,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+// GET Route for homepage
+app.get('/', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
